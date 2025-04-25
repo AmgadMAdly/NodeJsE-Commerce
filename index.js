7a7ffd43de22bd3ee11f4e6f6dee9b09f37bfcc6
Mahmoud <mahmoudabdou743@gmail.com>
2025-04-25 21:28:29 +0300

    "On progress Cart"

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/home', (req, res) => {
 
 })
 app.use('/api/User', UserRoutes);
-app.use('/api/Product',ProductRoutes , authMiddleware );
+// app.use('/api/Product',ProductRoutes , authMiddleware );
 
 
 
